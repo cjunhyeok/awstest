600f3fb6b91599155d2660aa69bf0e7106f2f9d3
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-11 13:55:04 +0900

    Create Domain Member

---
+++ src/main/java/com/aws/awstest/domain/Member.java
@@ -0,0 +1,32 @@
+package com.aws.awstest.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String username;
+    private String password;
+    private String nickname;
+
+    private String role;
+
+    @Builder
+    public Member(String username, String password, String nickname, String role) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
+}
