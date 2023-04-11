3704f1f31941cda1cfb124bbfa3aa5b891ac8b2e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-11 13:56:19 +0900

    Rename PK Column name

--- src/main/java/com/aws/awstest/domain/Member.java
+++ src/main/java/com/aws/awstest/domain/Member.java
@@ -1,5 +1,6 @@
 package com.aws.awstest.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
