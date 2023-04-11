5dac74826a3f370e700cb08b44806de08db5adfd
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-11 14:18:09 +0900

    Add findAllPostsWithMember

--- src/main/java/com/aws/awstest/repository/PostRepository.java
+++ src/main/java/com/aws/awstest/repository/PostRepository.java
@@ -1,7 +1,14 @@
 package com.aws.awstest.repository;
 
 import com.aws.awstest.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query(value = "select p from Post p join fetch p.member",
+    countQuery = "select p from Post p")
+    Page<Post> findAllPostsWithMember(Pageable pageable);
 }
