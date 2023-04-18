f483a59bfb0bc2faa02b437d1ec609e88de4e603
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-18 13:50:21 +0900

    Create MemberController with MemberForm

---
+++ src/main/java/com/aws/awstest/controller/MemberController.java
@@ -0,0 +1,39 @@
+package com.aws.awstest.controller;
+
+import com.aws.awstest.controller.form.MemberForm;
+import com.aws.awstest.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        memberService.join(form.getUsername(), form.getPassword(), form.getNickname());
+
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/aws/awstest/controller/form/MemberForm.java
@@ -0,0 +1,13 @@
+package com.aws.awstest.controller.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+
+    private String username;
+    private String password;
+    private String nickname;
+}
