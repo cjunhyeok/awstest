a91c68d8301cc5dc43705ecdf7a4295818c91a15
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-19 18:22:52 +0900

    Create Simple SecurityConfig with Spring Security

---
+++ src/main/java/com/aws/awstest/security/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.aws.awstest.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable(); // csrf 비활성화
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated();
+        http.formLogin();
+
+        return http.build();
+    }
+}
