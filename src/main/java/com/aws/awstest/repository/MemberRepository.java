4e328f0acf63ac2c8c2b3727a8b87a2112a85e4e
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-23 19:44:16 +0900

    Add findByUsername in MemberRepository

--- src/main/java/com/aws/awstest/repository/MemberRepository.java
+++ src/main/java/com/aws/awstest/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import com.aws.awstest.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByUsername(String username);
 }
