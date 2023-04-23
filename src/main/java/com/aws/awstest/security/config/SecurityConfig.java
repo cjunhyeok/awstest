8570f67b773087c7cf6fd1d37fd0419363b9e370
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-23 19:46:02 +0900

    Set UserDetailsService, AuthenticationProvider in SecurityConfig

--- src/main/java/com/aws/awstest/security/config/SecurityConfig.java
+++ src/main/java/com/aws/awstest/security/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.aws.awstest.security.config;
 
+import com.aws.awstest.repository.MemberRepository;
+import com.aws.awstest.security.provider.CustomAuthenticationProvider;
+import com.aws.awstest.security.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,23 +19,41 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final MemberRepository memberRepository;
+
+    String[] whiteList = {"/", "/login", "/members/new", "/posts"};
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf().disable(); // csrf 비활성화
 
         http.authorizeHttpRequests()
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/members/new").permitAll()
+                .requestMatchers(whiteList).permitAll()
                 .anyRequest().authenticated();
-        http.formLogin();
+
+        http.formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/");
+
+        http.userDetailsService(userDetailsService());
+        http.authenticationProvider(authenticationProvider());
 
         return http.build();
     }
     @Bean
-
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl(memberRepository);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new CustomAuthenticationProvider(userDetailsService(), passwordEncoder());
     }
 }
