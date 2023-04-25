bea85e762b45f0a31b45ed58d65ddcf995b4b28a
Choi jun hyoek <chlwnsgur1214@naver.com>
2023-04-25 16:44:01 +0900

    Add logout in HomeController

--- src/main/java/com/aws/awstest/controller/HomeController.java
+++ src/main/java/com/aws/awstest/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.aws.awstest.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -18,5 +23,16 @@ public class HomeController {
     @GetMapping("/login")
     public String login() {
         return "login/login";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/";
     }
 }
